5df3aafd92dcf692b32ed2f1037d2be6298085f2
matheusliesenberg <matheusliesenberg3@gmail.com>
2024-02-23 14:54:53 -0300

    Alteração de funcionamento e alteração no estilo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script setup>
 import { ref } from 'vue';
+
 const contador = ref(0)
 function descrementar(){
   contador.value--
 }
+function acrementar(){
+  contador.value++
+}
 </script>
 
 <template>
-<button @click="contador++">+</button>
+<button @click="acrementar">+</button>
 <button @click="descrementar">-</button>
-<p>valor do contator : {{contador }}</p>
+<p>valor do contator: {{contador }}</p>
 </template>
 
-<style scoped></style>
+<style scoped>
+button{
+  background-color: chocolate;
+  color: white;
+  font-weight: bolder;
+  border: 0;
+  margin: 2px;
+}
+</style>
