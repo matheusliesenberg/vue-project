48e929f4bad190afbe599c30787902a50788de57
matheusliesenberg <matheusliesenberg3@gmail.com>
2024-03-01 12:36:28 -0300

    alterações na descrementação e resetar

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,29 @@ import { ref } from 'vue';
 
 const contador = ref(0)
 function descrementar(){
-  if(acrementar.value > 0){
-    contador.value--
+  if(contador.value > 0){
+    contador.value--;
   }
 }
 function acrementar(){
-  contador.value++
+  contador.value++;
+}
+function resetar (){
+  contador.value = 0;
 }
 </script>
 
 <template>
 <button @click="acrementar">+</button>
 <button @click="descrementar">-</button>
+<button @click="resetar">Resetar</button>
 <p>valor do contator: {{contador }}</p>
 </template>
 
 <style scoped>
 button{
-  background-color: chocolate;
-  color: white;
+  background-color: rgb(255, 0, 0);
+  color: rgb(255, 255, 255);
   font-weight: bolder;
   border: 0;
   margin: 2px;
